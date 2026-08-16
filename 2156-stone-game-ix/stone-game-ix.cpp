ec4bc878a97cc115fc5f8e13736d29cd786a6cf6
Shreya Singh <145997075+shreyy02@users.noreply.github.com>
2026-08-16 18:03:49 +0530

    Time: 3 ms (75.38%) | Memory: 131.3 MB (69.29%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        vector<int> cnt(3, 0);
+
+        for(int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        if(cnt[0] % 2 == 0) {
+            return min(cnt[1], cnt[2]) > 0;
+        }
+
+        return abs(cnt[1] - cnt[2]) > 2;
+    }
+};
