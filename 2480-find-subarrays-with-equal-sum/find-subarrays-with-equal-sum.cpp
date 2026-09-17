aff92d869732bc806d8397e80db76f77462a4a18
Shreya Singh <145997075+shreyy02@users.noreply.github.com>
2026-09-17 21:53:05 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (24.27%) - LeetSync

---
+++ 2480-find-subarrays-with-equal-sum/find-subarrays-with-equal-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool findSubarrays(vector<int>& nums) {
+        unordered_set<int> st;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            int sum = nums[i] + nums[i + 1];
+
+            if (st.count(sum)) {
+                return true;
+            }
+
+            st.insert(sum);
+        }
+
+        return false;
+    }
+};
